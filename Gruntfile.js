3cdc5cb2b83ccae9a429fe0dc25b9c39cceb2526
Rudi Giesler <rudi@praekelt.com>
2014-09-22 16:33:10 +0200

    Use grunt test in package.json

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,6 @@
 module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-mocha-test');
-    grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-watch');
 
     grunt.initConfig({
--- utils/test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash -e
-jshint `find lib test examples -name "*.js"`
-mocha -R spec `find test examples/test -name "*.js"`
