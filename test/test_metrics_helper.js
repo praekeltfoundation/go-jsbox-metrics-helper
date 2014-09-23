df2f0a3092071e3294169705dee3fb1467562741
Rudi Giesler <rudi@praekelt.com>
2014-09-23 15:56:28 +0200

    Added tests for total_state_entries

--- test/test_metrics_helper.js
+++ test/test_metrics_helper.js
@@ -16,16 +16,26 @@ describe('MetricsHelper', function() {
 
         app.init = function() {
             metricsH = new MetricsHelper(app.im);
-            metricsH
+            return metricsH
                 .add.total_unique_users('uniqueUsers')
                 .add.total_unique_users()
                 .add.total_sessions('sessions')
-                .add.total_sessions('sessions2');
+                .add.total_sessions('sessions2')
+                .add.total_state_entries('states:test', 'entries')
+                .add.total_state_entries('states:test');
        };
 
         app.states.add('states:test', function(name) {
             return new EndState(name, {
-                text: 'This is the end state.'
+                text: 'This is the end state.',
+                next: 'states:test2'
+            });
+        });
+
+        app.states.add('states:test2', function(name) {
+            return new EndState(name, {
+                text: 'This is the end state.',
+                next: 'states:test'
             });
         });
 
@@ -142,6 +152,46 @@ describe('MetricsHelper', function() {
                 .check(function(api, im, app) {
                     metrics = api.metrics.stores['metricsHelper-tester'];
                     assert.equal(metrics.sessions, undefined);
+                })
+                .run();
+        });
+
+    });
+
+    describe('when the state is entered', function() {
+
+        it('should trigger the state enter metric', function() {
+            return tester
+                .start()
+                .check(function(api, im, app) {
+                    metrics = api.metrics
+                        .stores['metricsHelper-tester'].entries;
+                    assert.deepEqual(metrics, {agg: 'sum', values: [ 1 ]});
+                })
+                .run();
+        });
+
+        it('should trigger both state enter metrics', function() {
+            return tester
+                .start()
+                .check(function(api, im, app) {
+                    metric1 = api.metrics
+                        .stores['metricsHelper-tester'].entries;
+                    metric2 = api.metrics
+                        .stores['metricsHelper-tester'].total_state_entries;
+                    assert.deepEqual(metric1, {agg: 'sum', values: [ 1 ]});
+                    assert.deepEqual(metric2, {agg: 'sum', values: [ 1 ]});
+                })
+                .run();
+        });
+
+        it('should trigger each time the state is entered', function() {
+            return tester
+                .inputs(null, null)
+                .check(function(api, im, app) {
+                    metrics = api.metrics
+                        .stores['metricsHelper-tester'].entries;
+                    assert.deepEqual(metrics, {agg: 'sum', values: [ 1, 1 ]});
                 })
                 .run();
         });
